6b40ccc220a5adfa579c386aee1007c2f1a3d7c8
Gaspar Nicolas Viola <gaspar5454@hotmail.com>
2024-06-04 09:54:18 -0300

    Añadiendo Clase Conexion

---
+++ empresaColectivos-Lab1/src/Entidades/Conexion.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author El Notaloko 2.1
+ */
+public class Conexion {
+    private static String CLASS = "org.mariadb.jdbc.Driver";
+    private static String url="jdbc:mariadb://localhost/transpuntanosl";
+    private static String usuario="root";
+    private static String password="";
+
+
+    private static Conexion conexion=null;
+
+     private Conexion() {
+        try {
+            //1)Cargar Drivers.
+            Class.forName(CLASS);
+
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Clase Conexion: Error al cargar Driver");
+        }
+    }
+    public static Connection getConexion() {
+        Connection con=null;
+      if(conexion == null){
+
+           conexion= new Conexion();
+        }
+        try {
+            //2)Establecer la Conexion.
+            con = (Connection) DriverManager.getConnection(url + "?useLegacyDatetimeCode=false&serverTimezone=UTC" + "&user=" + usuario + "&password=" + password);
+
+        }catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion ");
+        }
+
+        return con;
+    }
+}
